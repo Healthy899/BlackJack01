fa94d728804086dfb242409e9bfc022a61509039
Jan Prchal <prchal.jan@outlook.com>
2018-09-24 09:25:59 +0100

    it kinda works... but still a lot to be done

--- BlackJack.py
+++ BlackJack.py
@@ -67,40 +67,142 @@ class Chips():
 
 
 def take_bet(chips):
-	pass
+	while chips.bet == 0 or chips.chips < chips.bet:
+		try:
+			chips.bet = int(input("Player, take a bet! You can bet 1 - {}".format(chips.chips)))
+		except:
+			print("Must be a number!")
+		else:
+			if chips.chips < chips.bet:
+				print("Not enough chips")
+				continue
+			else:
+				print("Player bets {}".format(chips.bet))
+	return chips.bet
+			
+		
+
 
 def take_hit(hand,deck):
-	pass
+	hand.add_card(deck.deal())
 
 def hit_or_stand(hand,deck):
-	pass
+	while hand.value < 21:
+		try:
+			play = str(input("Do you wish to Hit or Stand? ('h' or 's'"))
+		except:
+			print("Must be 'h' or 's'")
+		else:
+			if play[0].lower() == 'h':
+				take_hit(hand,deck)
+				hand.adjust_for_ace()
+				print("Player takes a hit")
+				continue
+			elif play[0].lower() == 's':
+				print("Player Stands!")
+				break
+			else:
+				print("I did not understand that! Try again!")
+				continue
 
 def display_some(player_hand,dealer_hand):
-	pass
+	print("Dealer's cards:")
+	print("HIDDEN CARD")
+	print(dealer_hand.cards[1])
+	print('\n')
+	print("Player's cards:")
+	print(player_hand.cards[0])
+	print(player_hand.cards[1])
+	print("Your current value is: {}".format(player_hand.value))
 
 def display_all(player_hand,dealer_hand):
-	pass
+	print("Dealer's card:")
+	print(dealer_hand.cards[0])
+	print(dealer_hand.cards[1])
+	print('\n')
+	print("Player's cards:")
+	print(player_hand.cards[0])
+	print(player_hand.cards[1])
+	print("Your current value is: {}".format(player_hand.value))
 
-def player_busts(player_hand,player_chips):
-	pass
-
-def dealer_busts(dealer_hand, player_chips):
-	pass
-
-def dealer_wins(player_hand,dealer_hand):
-	pass
+def dealer_turn(hand,deck):
+	while dealer_hand.value < 17:
+		take_hit(hand,deck)
+		hand.adjust_for_ace()
 
 
 
-x = Card('spades','two')
-print(x)
-y = Deck()
-y.shuffle()
-print(y)
-h = Hand()
-deal_card = y.deal()
-print("The card that has been dealt is:")
-print(deal_card)
-h.add_card(deal_card)
-print('The Value is:')
-print(h.value)
+def player_busts(player_hand,chips):
+	if player_hand.value > 21:
+		chips.value -= chips.bet
+	return player_hand.value > 21
+
+	
+
+
+def dealer_busts(dealer_hand,chips):
+	if dealer_hand.value > 21:
+		print("Dealer Busts!")
+		print("Player Wins!")
+		chips.chips += chips.bet
+		
+
+def dealer_wins(player_hand,dealer_hand,chips):
+	if dealer_hand.value > player_hand.value:
+		print("Dealer Wins!")
+		chips.value -= chips.bet
+
+
+def player_wins(player_hand, dealer_hand,chips):
+	if player_hand.value > dealer_hand.value:
+		print(player_wins)
+		chips.value += chips.bet
+
+
+
+
+while True:
+	# Set up the game:
+	deck = Deck()
+	deck.shuffle()
+
+	player_hand = Hand()
+	dealer_hand = Hand()
+	chips = Chips()
+
+	# Deal 2 cards to the player and dealer:
+	player_hand.add_card(deck.deal())
+	player_hand.add_card(deck.deal())
+
+	dealer_hand.add_card(deck.deal())
+	dealer_hand.add_card(deck.deal())
+
+	# Take a bet:
+	take_bet(chips)
+
+	# Adjust for Aces:
+	player_hand.adjust_for_ace()
+
+	# Show cards (hid one dealer's card)
+	display_some(player_hand,dealer_hand)
+
+	playing = True
+
+	while playing:
+		# Prompt player if he wants to Hit or Stand
+		hit_or_stand(player_hand,deck)
+
+		# Check is player busts:
+		if player_busts(player_hand,chips):
+			print("Player Busts!")
+			playing = False
+		# else dealer's turn:
+		else:
+			dealer_turn(dealer_hand,deck)
+			playing = False
+
+	# Check if dealer busts:
+	dealer_busts(dealer_hand,chips)
+
+
+
